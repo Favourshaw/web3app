bea54d08af45c68da7fa0b1f3fefa28b8311adaa
Favour Ebuka <favourvanderwaal@gmail.com>
2024-06-28 18:58:01 +0100

    transaction count enabled

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,7 +1,54 @@
+import { TransactionButton, useReadContract } from "thirdweb/react";
+import { CONTRACT } from "../utils/constants";
+import { prepareContractCall } from "thirdweb";
+
 const Counter: React.FC = () => {
+  const {
+    data: count,
+    isLoading: loadingCount,
+    refetch,
+  } = useReadContract({
+    contract: CONTRACT,
+    method: "getCount",
+  });
   return (
     <div>
       <h1>counter</h1>
+      {loadingCount ? <h2>...</h2> : <h2>{count?.toString()}</h2>}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "10px",
+          marginTop: "10px",
+        }}
+      >
+        <TransactionButton
+          transaction={() =>
+            prepareContractCall({
+              contract: CONTRACT,
+              method: "decrement",
+            })
+          }
+          onTransactionSent={() => console.log("decrementing...")}
+          onTransactionConfirmed={() => refetch()}
+        >
+          -
+        </TransactionButton>
+        <TransactionButton
+          transaction={() =>
+            prepareContractCall({
+              contract: CONTRACT,
+              method: "increment",
+            })
+          }
+          onTransactionSent={() => console.log("incrementing...")}
+          onTransactionConfirmed={() => refetch()}
+        >
+          +
+        </TransactionButton>
+      </div>
     </div>
   );
 };
